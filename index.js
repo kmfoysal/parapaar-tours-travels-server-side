ba05952c556ebece2377b2e6e8c73f549a0e8179
kmfoysal <faisal.cri@gmail.com>
2021-10-31 19:43:26 +0600

    add package intigrated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000 ;
 
 // Middleware 
@@ -28,14 +29,22 @@ async function run (){
         const bookingCollection = database.collection('bookings')
 
 
-        // GET Packages API 
+        // GET Packages Data API 
         app.get('/packages', async(req, res)=>{
             const cursor = packageCollection.find({});
             const packages = await cursor.toArray();
             res.send(packages);
         })
 
-         // Data Insert With POST  API 
+
+        // GET Bookings Data API 
+        app.get('/bookings', async(req, res)=>{
+            const cursor = bookingCollection.find({});
+            const bookings = await cursor.toArray();
+            res.send(bookings);
+        })
+
+         // Data Insert With POST  API for booking
 
          app.post('/bookings', async(req, res)=>{
             const newBooking = req.body;
@@ -45,6 +54,27 @@ async function run (){
             console.log(req.body);
             res.json(result);
   
+          })
+
+          // Data Insert With POST  API for Add Package
+
+         app.post('/packages', async(req, res)=>{
+            const newPackage = req.body;
+            const result = await packageCollection.insertOne(newPackage);
+            console.log(`A document was inserted with the _id: ${result.insertedId}`);
+            console.log(result); 
+            console.log(req.body);
+            res.json(result);
+  
+          })
+
+          // DELETE API 
+
+        app.delete('/bookings/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bookingCollection.deleteOne(query);
+            res.json(result);
           })
     }
     finally{
